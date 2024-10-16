ea1faa0bfa1a2e0ce000e08fc2051a22954e0402
Shivendra Patel <130781766+Shivaay3690@users.noreply.github.com>
2024-10-16 10:51:05 +0000

    feature of instagram story

---
+++ 07/logic.js
@@ -0,0 +1,23 @@
+var logic = [ 
+    {
+        dp:"https://plus.unsplash.com/premium_photo-1673758905770-a62f4309c43c?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",story:"https://plus.unsplash.com/premium_photo-1673758910817-42265a1693f8?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        dp: "https://plus.unsplash.com/premium_photo-1673758900167-cd6b427d313d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" , story:"https://plus.unsplash.com/premium_photo-1673758905600-4863f662bc1d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        dp:"https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",story:"https://images.unsplash.com/photo-1526413232644-8a40f03cc03b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        dp:"https://images.unsplash.com/photo-1601583844062-7487c80b56b7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",story:"https://images.unsplash.com/photo-1605677677382-8baa24578e08?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+]
+
+var clutter = ''
+logic.forEach(function(elem,idx){
+    clutter += ` <div id="story">
+                 <img id="${idx}" src="${elem.dp}" alt=""> 
+            </div>`
+    
+})
+document.querySelector('#storiyan').innerHTML = clutter
